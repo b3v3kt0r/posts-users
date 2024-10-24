851537415d765c434749dd382554075bc4c27419
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-10-24 19:46:53 +0300

    fix users services

--- users/services.py
+++ users/services.py
@@ -19,7 +19,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
