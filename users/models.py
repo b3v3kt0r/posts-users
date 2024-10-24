5243cc97a346f154f1c55897c2e6b9981c2874dc
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-10-24 13:30:31 +0300

    fix base model for user

--- users/models.py
+++ users/models.py
@@ -13,7 +13,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String(20), unique=True, nullable=False)
     email = Column(String(50), unique=True, nullable=False)
-    password = Column(String(50), nullable=False)
+    password = Column(String, nullable=False)
 
     posts = relationship("Post", back_populates="author")
     comments = relationship("Comment", back_populates="author")
